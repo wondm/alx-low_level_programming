aa36e62df07ae3f32533fac8801963abca26034c
wondm <wondez56@gmail.com>
2022-09-13 23:42:59 -0700

    c - variables if else

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char lowercase;
+
+	for (lowercase = 'a'; lowercase <= 'z'; lowercase++)
+		putchar(lowercase);
+
+	putchar('\n');
+
+	return (0);
+}
